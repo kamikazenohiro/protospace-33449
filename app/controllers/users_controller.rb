697c5bc21104d811f37bf434cd449d61cc6374e4
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-01-06 15:57:00 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @name = @user.name
+    @profile = @user.profile
+    @occupation = @user.occupation
+    @position = @user.position
+    @prototypes = @user.prototypes
+    #@user = User.includes(:user_params) #(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile, :occupation, :position)
+  end
+
+end
